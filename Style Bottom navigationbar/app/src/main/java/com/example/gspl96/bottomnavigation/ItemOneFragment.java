3b288192020c567707bac678018c02930d73b80c
prasanth <prasanth7431@gmail.com>
2019-01-28 19:45:59 +0530

    bottombar

---
+++ Style Bottom navigationbar/app/src/main/java/com/example/gspl96/bottomnavigation/ItemOneFragment.java
@@ -0,0 +1,24 @@
+package com.example.gspl96.bottomnavigation;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemOneFragment extends Fragment {
+    public static ItemOneFragment newInstance() {
+        ItemOneFragment fragment = new ItemOneFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_one, container, false);
+    }
+}
